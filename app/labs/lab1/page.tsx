82f9baf1f332142ab398013b57916e714a3ea813
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-01-12 16:32:10 -0500

    saving progress

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 export default function Lab1() {
   return (
     <div id="wd-lab1">
@@ -231,46 +232,43 @@ export default function Lab1() {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </textarea>
+      <h5 id="wd-buttons">Buttons</h5>
+      <button
+        type="button"
+        onClick={() => alert("Life is Good!")}
+        id="wd-all-good"
+      >
+        Hello World!
+      </button>
       <h5 id="wd-radio-buttons">Radio buttons</h5>
-
       <label>Favorite movie genre:</label>
       <br />
-
       <input type="radio" name="radio-genre" id="wd-radio-comedy" />
       <label htmlFor="wd-radio-comedy">Comedy</label>
       <br />
-
       <input type="radio" name="radio-genre" id="wd-radio-drama" />
       <label htmlFor="wd-radio-drama">Drama</label>
       <br />
-
       <input type="radio" name="radio-genre" id="wd-radio-scifi" />
       <label htmlFor="wd-radio-scifi">Science Fiction</label>
       <br />
       <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
       <label htmlFor="wd-radio-fantasy">Fantasy</label>
-
       <h5 id="wd-checkboxes">Checkboxes</h5>
       <label>Favorite movie genre:</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
       <label htmlFor="wd-chkbox-comedy">Comedy</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
       <label htmlFor="wd-chkbox-drama">Drama</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
       <label htmlFor="wd-chkbox-scifi">Science Fiction</label>
       <br />
-
       <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
       <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
-
       <h4 id="wd-dropdowns">Dropdowns</h4>
-
       <h5>Select one</h5>
       <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label>
       <br />
@@ -282,7 +280,6 @@ export default function Lab1() {
         </option>
         <option value="FANTASY">Fantasy</option>
       </select>
-
       <h5>Select many</h5>
       <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label>
       <br />
@@ -298,6 +295,50 @@ export default function Lab1() {
         </option>
         <option value="FANTASY"> Fantasy </option>
       </select>
+      <h4>Other HTML field types</h4>
+      <label htmlFor="wd-text-fields-email"> Email: </label>
+      <input
+        type="email"
+        placeholder="jdoe@somewhere.com"
+        id="wd-text-fields-email"
+      />
+      <br />
+      <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+      <input
+        type="number"
+        defaultValue="100000"
+        placeholder="1000"
+        id="wd-text-fields-salary-start"
+      />
+      <br />
+      <label htmlFor="wd-text-fields-rating"> Rating: </label>
+      <input
+        type="range"
+        defaultValue="4"
+        max="5"
+        placeholder="Doe"
+        id="wd-text-fields-rating"
+      />
+      <br />
+      <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+      <input type="date" defaultValue="2000-01-21" id="wd-text-fields-dob" />
+      <br />
+      <h4>Anchor tag</h4>
+      Please
+      <a href="https://www.lipsum.com" id="wd-lipsum">
+        click here
+      </a>
+      to get dummy text
+      <br />
+      Please
+      <a
+        href="https://www.github.com/estebanpuyanas/cs4550-webdev"
+        id="wd-github"
+      >
+        click here
+      </a>
+      to view the source code for this web application.
+      <br />
     </div>
   );
 }
