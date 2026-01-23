8279227a8b2336621ba9e81dc24368868267c60d
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-01-23 11:44:16 -0500

    starting lab 2

--- app/labs/lab2/page.tsx
+++ app/labs/lab2/page.tsx
@@ -24,6 +24,27 @@ export default function Lab2() {
                     This heading has the same style as the paragraph above.
                 </h4>
             </div>
+            <div id="wd-css-document-structure">
+                <div className="wd-selector-1">
+                    <h3>Document Structure Selectors</h3>
+                    <div className="wd-selector-1">
+                        Selectors can be combined to refer elements in particular
+                        places in the document.
+                        <p className="wd-selector-3">
+                            This paragraph's red background is referred as
+                            <br />
+                            .selector-2 .selector-3 <br />
+                            meaning the descendant of some ancestor. <br />
+                            <span className="wd-selector-4">
+                                Whereas this span is a direct child of its parent.
+                            </span><br />
+                            You can combine these relationships to create specific
+                            styles depending on the document structure.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
     </div>
   );
 }
