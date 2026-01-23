08919cf920ff3ac8a41f66ff19647a070d133528
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-01-23 11:02:13 -0500

    starting lab 2

--- app/labs/lab2/page.tsx
+++ app/labs/lab2/page.tsx
@@ -1,7 +1,14 @@
+import "./index.css"
 export default function Lab2() {
   return (
-    <div>
-      <h2>Lab 2</h2>
+    <div id="wd-lab2">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <h3>Styling with the STYLE Attribute</h3>
+            <p>
+                Style attributes allows configuring look and feel right on the element. 
+                Although it is very conveninent, it is considered bad practice and you should avoid 
+                using the style attribute.
+            </p>
     </div>
   );
 }
