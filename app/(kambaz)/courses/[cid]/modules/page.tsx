c217f16d5cb3606f9c0d8f81265caae0e4a8e40b
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-02-07 10:56:44 -0500

    updating modules secrtion for course, saving progress before refactor

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -1,4 +1,7 @@
 import ModulesControls from "./modulesControls";
+import ModuleControlButtons from "./moduleControlButtons";
+import LessonControlButtons from "./lessonControlButtons";
+import { BsGripVertical } from "react-icons/bs";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default function Modules() {
@@ -8,49 +11,66 @@ export default function Modules() {
       <br />
       <br />
       <br />
-      <br />
       <ListGroup className="rounded-0" id="wd-modules">
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 1{" "}
+            <ModuleControlButtons />
+          </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              LEARNING OBJECTIVES{" "}
+              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES{" "}
+              <LessonControlButtons />
             </ListGroupItem>
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              Introduction to the course{" "}
+              <BsGripVertical className="me-2 fs-3" /> Introduction to the
+              course <LessonControlButtons />
             </ListGroupItem>
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              Learn what is Web Development{" "}
+              <BsGripVertical className="me-2 fs-3" /> What is web development?{" "}
+              <LessonControlButtons />
             </ListGroupItem>
           </ListGroup>
         </ListGroupItem>
 
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 2{" "}
+            <ModuleControlButtons />
+          </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              LEARNING OBJECTIVES{" "}
+              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES{" "}
+              <LessonControlButtons />
             </ListGroupItem>
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              The History of HTML{" "}
+              <BsGripVertical className="me-2 fs-3" /> The History of HTML{" "}
+              <LessonControlButtons />
             </ListGroupItem>
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              Using CSS to Style Web Applications{" "}
+              <BsGripVertical className="me-2 fs-3" /> Styling Web Apps with CSS{" "}
+              <LessonControlButtons />
             </ListGroupItem>
           </ListGroup>
         </ListGroupItem>
 
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> Week 3 </div>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 3{" "}
+            <ModuleControlButtons />
+          </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              LEARNING OBJECTIVES{" "}
+              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES{" "}
+              <LessonControlButtons />
             </ListGroupItem>
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              Dynamic Data Handling with TypeScript{" "}
+              <BsGripVertical className="me-2 fs-3" /> Dyanmic Data Handling
+              with TypeScript <LessonControlButtons />
             </ListGroupItem>
             <ListGroupItem className="wd-lesson p-3 ps-1">
-              What is the ORM?{" "}
+              <BsGripVertical className="me-2 fs-3" /> The ORM{" "}
+              <LessonControlButtons />
             </ListGroupItem>
           </ListGroup>
         </ListGroupItem>
