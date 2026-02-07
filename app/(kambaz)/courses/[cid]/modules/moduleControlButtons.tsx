8483bef9d2194cd996db9b30a31780b278245d4a
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-02-07 11:20:49 -0500

    amen

--- app/(kambaz)/courses/[cid]/modules/moduleControlButtons.tsx
+++ app/(kambaz)/courses/[cid]/modules/moduleControlButtons.tsx
@@ -4,7 +4,7 @@ import GreenCheckmark from "./GreenCheckmark";
 
 export default function ModuleControlButtons() {
   return (
-    <div className="d-flex align-items-center">
+    <div className="float-end">
       <GreenCheckmark />
       <BsPlus className="fs-4" />
       <IoEllipsisVertical className="fs-4" />
