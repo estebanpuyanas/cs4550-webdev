3b0244ab43af3f8cf78305d10f8e121fac01ade4
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-02-13 11:32:16 -0500

    up to 3.4.7 (exclusive) filter function

--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -1,7 +1,40 @@
+import VariableTypes from './VariablesAndConstants';
+import IfElse from './IfElse';
+import TernaryOperator from './TernaryOperator';
+import ConditionalOutputIfElse from './ConditionalOutputIfElse';
+import ConditionalOutputInline from './ConditionalOutputInline';
+import LegacyFunctions from './LegacyFunctions';
+import ArrowFunctions from './ArrowFunctions';
+import ImpliedReturn from './ImpliedReturn';
+import TemplateLiterals from './TemplateLiterals';
+import SimpleArrays from './SimpleArrays';
+import ArrayIndexAndLength from './ArrayIndexAndLength';
+import AddingAndRemovingToFromArrays from './AddingAndRemovingToFromArrays';
+import ForLoops from './ForLoops';
+import MapFunction from './MapFunction';
+import FindFunction from './FindFunction';
+import FindIndex from './FindIndex';
+
 export default function Lab3() {
   return (
     <div>
       <h2>Lab 3</h2>
+      <VariableTypes />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
     </div>
   );
 }
