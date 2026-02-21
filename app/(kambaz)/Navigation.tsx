158c33b248e9fd8b0673d30521338224f77563ec
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-02-21 10:45:59 -0500

    starting 3.10.5

--- app/(kambaz)/Navigation.tsx
+++ app/(kambaz)/Navigation.tsx
@@ -1,89 +1,63 @@
-import { AiOutlineDashboard } from "react-icons/ai";
-import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
-import Link from "next/link";
+import { AiOutlineDashboard } from 'react-icons/ai';
+import { IoCalendarOutline } from 'react-icons/io5';
+import { LiaBookSolid, LiaCogSolid } from 'react-icons/lia';
+import { FaInbox, FaRegCircleUser } from 'react-icons/fa6';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import Link from 'next/link';
 
 export default function KambazNavigation() {
   return (
     <ListGroup
-      id="wd-kambaz-navigation"
+      id='wd-kambaz-navigation'
       style={{ width: 110 }}
-      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
-    >
+      className='rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2'>
       <ListGroupItem
-        className="bg-black border-0 text-center"
-        as="a"
-        target="_blank"
-        href="https://www.northeastern.edu/"
-        id="wd-neu-link"
-      >
-        <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
+        className='bg-black border-0 text-center'
+        as='a'
+        target='_blank'
+        href='https://www.northeastern.edu/'
+        id='wd-neu-link'>
+        <img src='/images/NEU.png' width='75px' alt='Northeastern University' />
       </ListGroupItem>
 
-      <ListGroupItem className="border-0 bg-black text-center">
-        <Link
-          href="/account"
-          id="wd-account-link"
-          className="text-white text-decoration-none"
-        >
-          <FaRegCircleUser className="fs-1 text-white" />
+      <ListGroupItem className='border-0 bg-black text-center'>
+        <Link href='/account' id='wd-account-link' className='text-white text-decoration-none'>
+          <FaRegCircleUser className='fs-1 text-white' />
           Account
         </Link>
       </ListGroupItem>
 
-      <ListGroupItem className="border-0 bg-white text-center">
-        <Link
-          href="/dashboard"
-          id="wd-dashboard-link"
-          className="text-danger text-decoration-none"
-        >
-          <AiOutlineDashboard className="fs-1 text-danger" />
+      <ListGroupItem className='border-0 bg-white text-center'>
+        <Link href='/dashboard' id='wd-dashboard-link' className='text-danger text-decoration-none'>
+          <AiOutlineDashboard className='fs-1 text-danger' />
           Dashboard
         </Link>
       </ListGroupItem>
 
-      <ListGroupItem className="border-0 bg-black text-center">
-        <Link
-          href="/courses"
-          id="wd-courses-link"
-          className="text-white text-decoration-none"
-        >
-          <LiaBookSolid className="fs-1 text-danger" />
+      <ListGroupItem className='border-0 bg-black text-center'>
+        <Link href='/courses' id='wd-courses-link' className='text-white text-decoration-none'>
+          <LiaBookSolid className='fs-1 text-danger' />
           Courses
         </Link>
       </ListGroupItem>
 
-      <ListGroupItem className="border-0 bg-black text-center">
-        <Link
-          href="/calendar"
-          id="wd-calendar-link"
-          className="text-white text-decoration-none"
-        >
-          <IoCalendarOutline className="fs-1 text-danger" />
+      <ListGroupItem className='border-0 bg-black text-center'>
+        <Link href='/calendar' id='wd-calendar-link' className='text-white text-decoration-none'>
+          <IoCalendarOutline className='fs-1 text-danger' />
           Calendar
         </Link>
       </ListGroupItem>
 
-      <ListGroupItem className="border-0 bg-black text-center">
-        <Link
-          href="/inbox"
-          id="wd-inbox-link"
-          className="text-white text-decoration-none"
-        >
-          <FaInbox className="fs-1 text-danger" />
+      <ListGroupItem className='border-0 bg-black text-center'>
+        <Link href='/inbox' id='wd-inbox-link' className='text-white text-decoration-none'>
+          <FaInbox className='fs-1 text-danger' />
           Inbox
         </Link>
       </ListGroupItem>
 
-      <ListGroupItem className="border-0 bg-black text-center">
-        <Link
-          href="/labs"
-          id="wd-labs-link"
-          className="text-white text-decoration-none"
-        >
-          <LiaCogSolid className="fs-1 text-danger" />
+      <ListGroupItem className='border-0 bg-black text-center'>
+        <Link href='/labs' id='wd-labs-link' className='text-white text-decoration-none'>
+          <LiaCogSolid className='fs-1 text-danger' />
           Labs
         </Link>
       </ListGroupItem>
