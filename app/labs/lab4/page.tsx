3aaca99e3b44ed4ee20f7ea06c502b9dabbe9a1d
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-02-21 22:39:31 -0500

    up to 4.3.1 (not inclusive)

---
+++ app/labs/lab4/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
+
+import Link from "next/link";
+
+export default function Lab4() {
+    function sayHello () {
+        alert("Hello");
+    }
+    return (
+    <div id="wd-lab4" className="container">
+            <h2>
+                Lab 4
+            </h2>
+            <ClickEvent/>
+            <PassingDataOnEvent/>
+            <PassingFunctions theFunction={sayHello}/>
+            <Counter/>
+            <BooleanStateVariables></BooleanStateVariables>
+            <StringStateVariables></StringStateVariables>
+            <DateStateVariable>This needs to be fixed so make sure to check piazza</DateStateVariable>
+            <ObjectStateVariable></ObjectStateVariable>
+            <ArrayStateVariable></ArrayStateVariable>
+            <ParentStateComponent></ParentStateComponent>
+
+            <Link href="./../lab4/redux/">Redux Examples</Link>
+        </div>
+    );
+}
