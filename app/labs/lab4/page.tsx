fdad8bf4ec839e537de753ddfec3854f2ae94eb8
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-02-25 15:23:30 -0500

    progress on 4.4.x

--- app/labs/lab4/page.tsx
+++ app/labs/lab4/page.tsx
@@ -1,42 +1,41 @@
-"use client";
-import ArrayStateVariable from "./ArrayStateVariable";
-import BooleanStateVariables from "./BooleanStateVariables";
-import ClickEvent from "./ClickEvent";
-import Counter from "./Counter";
-import DateStateVariable from "./DateStateVariable";
-import ObjectStateVariable from "./ObjectStateVariable";
-import ParentStateComponent from "./ParentStateComponent";
-import PassingDataOnEvent from "./PassingDataOnEvent";
-import PassingFunctions from "./PassingFunctions";
-import StringStateVariables from "./StringStateVariables";
-import store from "./store";
-import { Provider } from "react-redux";
+'use client';
+import ArrayStateVariable from './ArrayStateVariable';
+import BooleanStateVariables from './BooleanStateVariables';
+import ClickEvent from './ClickEvent';
+import Counter from './Counter';
+import DateStateVariable from './DateStateVariable';
+import ObjectStateVariable from './ObjectStateVariable';
+import ParentStateComponent from './ParentStateComponent';
+import PassingDataOnEvent from './PassingDataOnEvent';
+import PassingFunctions from './PassingFunctions';
+import StringStateVariables from './StringStateVariables';
+import store from './store';
+import { Provider } from 'react-redux';
 
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Lab4() {
-    function sayHello () {
-        alert("Hello");
-    }
-    return (
-        <Provider store={store}>
-    <div id="wd-lab4" className="container">
-            <h2>
-                Lab 4
-            </h2>
-            <ClickEvent/>
-            <PassingDataOnEvent/>
-            <PassingFunctions theFunction={sayHello}/>
-            <Counter/>
-            <BooleanStateVariables></BooleanStateVariables>
-            <StringStateVariables></StringStateVariables>
-            <DateStateVariable>This needs to be fixed so make sure to check piazza</DateStateVariable>
-            <ObjectStateVariable></ObjectStateVariable>
-            <ArrayStateVariable></ArrayStateVariable>
-            <ParentStateComponent></ParentStateComponent>
-
-            <Link href="./../lab4/redux/">Redux Examples</Link>
-        </div>
-        </Provider>
-    );
+  function sayHello() {
+    alert('Hello');
+  }
+  return (
+    <Provider store={store}>
+      <div id='wd-lab4' className='container'>
+        <h2>Lab 4</h2>
+        <ClickEvent />
+        <PassingDataOnEvent />
+        <PassingFunctions theFunction={sayHello} />
+        <Counter />
+        <BooleanStateVariables></BooleanStateVariables>
+        <StringStateVariables></StringStateVariables>
+        <DateStateVariable>This needs to be fixed so make sure to check piazza</DateStateVariable>
+        <ObjectStateVariable></ObjectStateVariable>
+        <ArrayStateVariable></ArrayStateVariable>
+        <ParentStateComponent></ParentStateComponent>
+        <Link href='./../lab4/redux/'>Redux Examples</Link>
+        <hr />
+        <Link href='./lab4/react-context'>React context examples</Link>
+      </div>
+    </Provider>
+  );
 }
--- app/labs/lab4/redux/page.tsx
+++ app/labs/lab4/redux/page.tsx
@@ -1,7 +1,14 @@
+import AddRedux from './AddRedux';
+import CounterRedux from './CounterRedux';
+import TodoList from './todos/TodoForm';
 export default function ReduxExamples() {
   return (
     <div>
       <h2>Redux Examples</h2>
+      {/* Not sure if this is the right way? Also, how do I import hello? */}
+      <AddRedux />
+      <CounterRedux />
+      <TodoList />
     </div>
   );
 }
