cea9d1e0a6003065ec0d2b11614a74d9dfd524e6
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-03-09 23:02:55 -0400

    fixing up random stuff

--- app/(kambaz)/courses/[cid]/layout.tsx
+++ app/(kambaz)/courses/[cid]/layout.tsx
@@ -1,21 +1,26 @@
 'use client';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import CourseNavigation from './Navigation';
 import { FaAlignJustify } from 'react-icons/fa';
 import Breadcrumb from './Breadcrumb';
 import { useSelector } from 'react-redux';
 import { useParams } from 'next/navigation';
-import { RootState } from '@reduxjs/toolkit/query';
+import { RootState } from '../../store';
 
 export default function CoursesLayout({ children }: { children: ReactNode }) {
   const { cid } = useParams();
   const { courses } = useSelector((state: RootState) => state.coursesReducer);
   const course = courses.find((course: any) => course._id === cid);
+  const [navVisible, setNavVisible] = useState(true);
 
   return (
     <div id='wd-courses'>
       <div className='d-flex align-items-center gap-3 text-danger'>
-        <FaAlignJustify className='fs-4' />
+        <FaAlignJustify
+          className='fs-4'
+          style={{ cursor: 'pointer' }}
+          onClick={() => setNavVisible(v => !v)}
+        />
         {course?.name}
         <h2 className='m-0 fs-5'>
           <Breadcrumb course={course} />
@@ -23,9 +28,11 @@ export default function CoursesLayout({ children }: { children: ReactNode }) {
       </div>
       <hr />
       <div className='d-flex'>
-        <div className='d-none d-md-block'>
-          <CourseNavigation cid={cid} />
-        </div>
+        {navVisible && (
+          <div className='d-none d-md-block'>
+            <CourseNavigation cid={cid as string} />
+          </div>
+        )}
         <div className='flex-fill'>{children}</div>
       </div>
     </div>
