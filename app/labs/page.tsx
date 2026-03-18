f35be4f76bda8e6ced7393a754ca9dbdb5dd1259
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-03-18 10:33:59 -0400

    up to 5.2.3 exclusive

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -1,34 +1,39 @@
-import Link from "next/link";
+import Link from 'next/link';
 export default function labs() {
   return (
-    <div id="wd-labs">
+    <div id='wd-labs'>
       <h1>Labs</h1>
       <h2>Esteban Puyana Salazar</h2>
       <ul>
         <li>
-          <Link href="/labs/lab1" id="wd-lab1-link">
-            Lab 1: HTML Examples{" "}
+          <Link href='/labs/lab1' id='wd-lab1-link'>
+            Lab 1: HTML Examples{' '}
           </Link>
         </li>
         <li>
-          <Link href="/labs/lab2" id="wd-lab2-link">
-            Lab 2: CSS Basics{" "}
+          <Link href='/labs/lab2' id='wd-lab2-link'>
+            Lab 2: CSS Basics{' '}
           </Link>
         </li>
         <li>
-          <Link href="/labs/lab3" id="wd-lab3-link">
-            Lab 3: JavaScript Fundamentals{" "}
+          <Link href='/labs/lab3' id='wd-lab3-link'>
+            Lab 3: JavaScript Fundamentals{' '}
           </Link>
         </li>
         <li>
-          <Link href="/labs/lab4" id="wd-lab4-link">
-            Lab 4: State Management{" "}
+          <Link href='/labs/lab4' id='wd-lab4-link'>
+            Lab 4: State Management{' '}
           </Link>
         </li>
         <li>
-          <Link href="/" id="kambaz-home">
-            Kambaz{" "}
-          </Link>{" "}
+          <Link href='/labs/lab5' id='wd-lab5-link'>
+            Lab 5: Implementing RESTful Web APIs with Express.js{' '}
+          </Link>
+        </li>
+        <li>
+          <Link href='/' id='kambaz-home'>
+            Kambaz{' '}
+          </Link>{' '}
         </li>
       </ul>
     </div>
