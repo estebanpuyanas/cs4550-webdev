492411c76bc1d596b5e8b2352fa5a050542e80c7
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2026-03-18 20:25:13 -0400

    up to 5.26 exclusive

--- app/labs/lab5/page.tsx
+++ app/labs/lab5/page.tsx
@@ -4,6 +4,9 @@ import PathParameters from './PathParameters';
 import QueryParameters from './QueryParameters';
 import WorkingWithObjects from './WorkingWithObjects';
 import WorkingWithArrays from './WorkingWithArrays';
+import HttpClient from './HttpClient';
+import WorkingWithObjectsAsynchronously from './WorkingWithObjectsAsynchronously';
+import WorkingWithArraysAsynchronously from './WorkingWithArraysAsynchronously';
 
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 
@@ -22,6 +25,9 @@ export default function Lab5() {
       <QueryParameters />
       <WorkingWithObjects />
       <WorkingWithArrays />
+      <HttpClient />
+      <WorkingWithObjectsAsynchronously />
+      <WorkingWithArraysAsynchronously />
     </div>
   );
 }
